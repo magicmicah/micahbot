4507e8b3913e072e5f5a66a98eaabd4848c1976e
svcCWxMlvGitHub <Micah.Magruder@Cerner.com>
2021-08-18 15:13:26 -0400

    back to og print

--- micahbot.py
+++ micahbot.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {0.user}.")
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(self, message):
